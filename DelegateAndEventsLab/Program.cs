9d0f05c1579a60d4883b45f3f3eccdfa9fec787d
PiterPoker <tema5286@gmail.com>
2024-12-13 23:59:22 +0300

    feat: Add GetMax and GetMin extension methods to IEnumerable

--- DelegateAndEventsLab/Program.cs
+++ DelegateAndEventsLab/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using MaxFinderWithEvents;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Rectangle> rectangles =
+        [
+            new Rectangle(5.5f, 3.2f),
+            new Rectangle(7.1f, 4.8f),
+            new Rectangle(2.2f, 2.2f),
+            new Rectangle(10.0f, 6.5f),
+            new Rectangle(1.5f, 9.9f),
+        ];
+        var rectangle = rectangles.GetMax(c => c.Perimeter);
+        Console.WriteLine("Hello, World!");
+    }
+}
--- MaxFinderWithEvents/EnumerableExtensions.cs
+++ MaxFinderWithEvents/EnumerableExtensions.cs
@@ -1,12 +1,22 @@
 ﻿using System.Runtime.InteropServices;
 
 namespace MaxFinderWithEvents;
-
+/// <summary>
+/// Provides extension methods for <see cref="IEnumerable{T}"/>.
+/// </summary>
 public static class EnumerableExtensions
 {
+    /// <summary>
+    /// Finds the item in the collection that yields the maximum value when passed through a conversion function.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the collection. Must be a class.</typeparam>
+    /// <param name="collection">The input collection.</param>
+    /// <param name="convertToNumber">A function that converts an item of type <typeparamref name="T"/> to a float value.</param>
+    /// <returns>The item in the collection that produces the maximum converted value.</returns>
+    /// <exception cref="ArgumentException">Thrown if the collection is null or empty.</exception>
     public static T GetMax<T>(this IEnumerable<T> collection, Func<T, float> convertToNumber) where T : class
     {
-        if (collection == null || !collection.Any())
+        if (collection == null)
             throw new ArgumentException("The collection cannot be null or empty");
 
         var enumerator = collection.GetEnumerator();
@@ -28,5 +38,40 @@ public static class EnumerableExtensions
         }
 
         return maxItem;
+    }
+
+    /// <summary>
+    /// Finds the item in the collection that yields the minimum value when passed through a conversion function.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the collection. Must be a class.</typeparam>
+    /// <param name="collection">The input collection.</param>
+    /// <param name="convertToNumber">A function that converts an item of type <typeparamref name="T"/> to a float value.</param>
+    /// <returns>The item in the collection that produces the minimum converted value.</returns>
+    /// <exception cref="ArgumentException">Thrown if the collection is null or empty.</exception>
+
+    public static T GetMin<T>(this IEnumerable<T> collection, Func<T, float> convertToNumber) where T : class
+    {
+        if (collection == null)
+            throw new ArgumentException("The collection cannot be null or empty");
+
+        var enumerator = collection.GetEnumerator();
+        if (!enumerator.MoveNext())
+            throw new ArgumentException("The collection cannot be empty");
+
+        T minItem = enumerator.Current;
+        float minValue = convertToNumber(minItem);
+
+        while (enumerator.MoveNext())
+        {
+            var item = enumerator.Current;
+            var value = convertToNumber(item);
+            if (value < minValue)
+            {
+                minValue = value;
+                minItem = item;
+            }
+        }
+
+        return minItem;
     }
 }
